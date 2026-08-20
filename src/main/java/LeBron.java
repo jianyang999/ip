3a52548aadb5f73cf3cc3089af49f495569e3ec2
Jian Yang <126151808+jianyang999@users.noreply.github.com>
2026-08-20 22:57:19 +0800

    Add list feature
    Users can now use LeBron to keep track and list items.

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class LeBron {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
 
         //Initial message
         String banner = "____________________________________________________________\n" +
                 "What's up, I am LeBron.\n" +
                 "Can the king assist you in any way?\n" +
-                "____________________________________________________________\n";
+                "____________________________________________________________";
         System.out.println(banner);
 
         //Echo logic
@@ -18,10 +20,17 @@ public class LeBron {
 
             //If user wants to leave
             if (input.equals("bye")){
-                System.out.println("    " + "Peace out fam!\n" + "____________________________________________________________\n");
+                System.out.println("    " + "Peace out fam!\n" + "____________________________________________________________");
                 break;
+            } else if(input.equals("list")){
+                for (int i = 0; i < counter; i++){
+                    System.out.println("    " + (i+1) + ") " + list[i]);
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                System.out.println("    " + input + "\n" + "____________________________________________________________\n");
+                list[counter] = input;
+                counter++;
+                System.out.println("    " + "Added: " + input + "\n" + "____________________________________________________________");
             }
         }
 
