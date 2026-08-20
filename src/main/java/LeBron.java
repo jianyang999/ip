23f49c6abf154679b3d99c730aaa791062eeb2ba
Jian Yang <126151808+jianyang999@users.noreply.github.com>
2026-08-20 22:09:37 +0800

    Change greeting
    Print a greet message on start

---
+++ src/main/java/LeBron.java
@@ -0,0 +1,10 @@
+public class LeBron {
+    public static void main(String[] args) {
+        String banner = "____________________________________________________________\n" +
+                "What's up, I am LeBron.\n" +
+                "Can the king assist you in any way?\n" +
+                "____________________________________________________________\n" +
+                "PEACE!";
+        System.out.println(banner);
+    }
+}
