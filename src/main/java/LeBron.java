0264c2f0fbe7e8c0eb00b37755844e7bdbdb97d9
Jian Yang <126151808+jianyang999@users.noreply.github.com>
2026-08-21 12:24:08 +0800

    Added CommandType enum
    Previously input string was manually detected using massive else-if blocks. It is changed to now use an enum called CommandType, used to group input types together.

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -2,6 +2,31 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class LeBron {
+
+    private static CommandType getCommandType(String input) {
+        String firstWord = input.split(" ")[0];
+        switch (firstWord) {
+            case "bye":
+                return CommandType.BYE;
+            case "list":
+                return CommandType.LIST;
+            case "mark":
+                return CommandType.MARK;
+            case "unmark":
+                return CommandType.UNMARK;
+            case "todo":
+                return CommandType.TODO;
+            case "deadline":
+                return CommandType.DEADLINE;
+            case "event":
+                return CommandType.EVENT;
+            case "delete":
+                return CommandType.DELETE;
+            default:
+                return CommandType.UNKNOWN;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> list = new ArrayList<>();
@@ -16,77 +41,87 @@ public class LeBron {
         while (true){
             //Read user input
             String input = scanner.nextLine();
+            CommandType commandType = getCommandType(input);
 
             try {
-                if (input.equals("bye")){
-                    System.out.println("    " + "Peace out fam!\n" + "____________________________________________________________");
-                    break;
-                } else if(input.equals("list")){
-                    for (int i = 0; i < list.size(); i++){
-                        System.out.println("    " + (i+1) + ") " + list.get(i));
+                switch (commandType) {
+                    case BYE -> {
+                        System.out.println("    " + "Peace out fam!\n" + "____________________________________________________________");
+                        scanner.close();
+                        return;
                     }
-                    System.out.println("____________________________________________________________");
-                } else if(input.startsWith("mark ")){
-                    int taskNumber = Integer.parseInt((input.split(" "))[1]);
-                    int index = taskNumber - 1;
-                    list.get(index).setStatus(true);
-                    System.out.println("    " +  "Oh yea we're striving for greatness!\n" + list.get(index));
-                    System.out.println("____________________________________________________________");
-                } else if (input.startsWith("unmark ")){
-                    int taskNumber = Integer.parseInt((input.split(" "))[1]);
-                    int index = taskNumber - 1;
-                    list.get(index).setStatus(false);
-                    System.out.println("    " +  "Oh nah we undoing stuff now?\n" + list.get(index));
-                    System.out.println("____________________________________________________________");
-                } else if(input.equals("todo") || input.startsWith("todo ")){
-                    String description = input.length() > 4 ? input.substring(5).trim() : "";
-                    if (description.isEmpty()) {
-                        throw new LeBronException("Whatchu need to do?");
+                    case LIST -> {
+                        for (int i = 0; i < list.size(); i++){
+                            System.out.println("    " + (i+1) + ") " + list.get(i));
+                        }
+                        System.out.println("____________________________________________________________");
                     }
-                    list.add(new Todo(description));
-                    System.out.println("    More todo!");
-                    System.out.println("    " + list.get(list.size() - 1));
-                    System.out.println("    " + list.size() + " tasks in your grind list now!");
-                    System.out.println("____________________________________________________________");
-                } else if(input.equals("deadline") || input.startsWith("deadline ")){
-                    String fullDesc = input.length() > 8 ? input.substring(9).trim() : "";
-                    if (fullDesc.isEmpty()) {
-                        throw new LeBronException("Yo specify your deadline!");
+                    case MARK -> {
+                        int taskNumber = Integer.parseInt((input.split(" "))[1]);
+                        int index = taskNumber - 1;
+                        list.get(index).setStatus(true);
+                        System.out.println("    " +  "Oh yea we're striving for greatness!\n" + "    " + list.get(index));
+                        System.out.println("____________________________________________________________");
                     }
-                    if (!fullDesc.contains(" by ")) {
-                        throw new LeBronException("Do not test me kid, specify your due date!");
+                    case UNMARK -> {
+                        int taskNumber = Integer.parseInt((input.split(" "))[1]);
+                        int index = taskNumber - 1;
+                        list.get(index).setStatus(false);
+                        System.out.println("    " +  "Oh nah we undoing stuff now?\n" + "    " + list.get(index));
+                        System.out.println("____________________________________________________________");
                     }
-                    String[] parts = fullDesc.split(" by ");
-                    list.add(new Deadline(parts[0], parts[1]));
-                    System.out.println("    " + "Deadlines forge kings!");
-                    System.out.println("    " + list.get(list.size() - 1));
-                    System.out.println("    " + list.size() + " tasks in your grind list now!");
-                    System.out.println("____________________________________________________________");
-                } else if(input.equals("event") || input.startsWith("event ")){
-                    String fullDesc = input.length() > 5 ? input.substring(6).trim() : "";
-                    if (fullDesc.isEmpty()) {
-                        throw new LeBronException("What event you tryna do?");
+                    case TODO -> {
+                        String description = input.length() > 4 ? input.substring(5).trim() : "";
+                        if (description.isEmpty()) {
+                            throw new LeBronException("Whatchu need to do?");
+                        }
+                        list.add(new Todo(description));
+                        System.out.println("    More todo!");
+                        System.out.println("    " + list.get(list.size() - 1));
+                        System.out.println("    " + list.size() + " tasks in your grind list now!");
+                        System.out.println("____________________________________________________________");
                     }
-                    if (!fullDesc.contains(" from ") || !fullDesc.contains(" to ")) {
-                        throw new LeBronException("Please tell me when it starts and ends little one.");
+                    case DEADLINE -> {
+                        String fullDesc = input.length() > 8 ? input.substring(9).trim() : "";
+                        if (fullDesc.isEmpty()) {
+                            throw new LeBronException("Yo specify your deadline!");
+                        }
+                        if (!fullDesc.contains(" by ")) {
+                            throw new LeBronException("Do not test me kid, specify your due date!");
+                        }
+                        String[] parts = fullDesc.split(" by ");
+                        list.add(new Deadline(parts[0], parts[1]));
+                        System.out.println("    " + "Deadlines forge kings!");
+                        System.out.println("    " + list.get(list.size() - 1));
+                        System.out.println("    " + list.size() + " tasks in your grind list now!");
+                        System.out.println("____________________________________________________________");
                     }
-                    String[] parts = fullDesc.split(" from | to ");
-                    list.add(new Event(parts[0], parts[1], parts[2]));
-                    System.out.println("    " + "Event fit for a king!");
-                    System.out.println("    " + list.get(list.size() - 1));
-                    System.out.println("    " + list.size() + " tasks in your grind list now!");
-                    System.out.println("____________________________________________________________");
-                } else if(input.startsWith("delete ")){
-                    int taskNumber = Integer.parseInt((input.split(" "))[1]);
-                    int index = taskNumber - 1;
-                    Task removed = list.get(index);
-                    list.remove(index);
-                    System.out.println("    Task been taken care of!");
-                    System.out.println("    " + removed);
-                    System.out.println("    " + list.size() + " tasks left kiddo.");
-                    System.out.println("____________________________________________________________");
-                } else {
-                    throw new LeBronException("Whatchu tryna do youngblood?");
+                    case EVENT -> {
+                        String fullDesc = input.length() > 5 ? input.substring(6).trim() : "";
+                        if (fullDesc.isEmpty()) {
+                            throw new LeBronException("What event you tryna do?");
+                        }
+                        if (!fullDesc.contains(" from ") || !fullDesc.contains(" to ")) {
+                            throw new LeBronException("Please tell me when it starts and ends little one.");
+                        }
+                        String[] parts = fullDesc.split(" from | to ");
+                        list.add(new Event(parts[0], parts[1], parts[2]));
+                        System.out.println("    " + "Event fit for a king!");
+                        System.out.println("    " + list.get(list.size() - 1));
+                        System.out.println("    " + list.size() + " tasks in your grind list now!");
+                        System.out.println("____________________________________________________________");
+                    }
+                    case DELETE -> {
+                        int taskNumber = Integer.parseInt((input.split(" "))[1]);
+                        int index = taskNumber - 1;
+                        Task removed = list.get(index);
+                        list.remove(index);
+                        System.out.println("    Task been taken care of!");
+                        System.out.println("    " + removed);
+                        System.out.println("    " + list.size() + " tasks left kiddo.");
+                        System.out.println("____________________________________________________________");
+                    }
+                    default -> throw new LeBronException("Whatchu tryna do youngblood?");
                 }
             } catch (LeBronException e) {
                 System.out.println("    " + e.getMessage());
@@ -99,8 +134,5 @@ public class LeBron {
                 System.out.println("____________________________________________________________");
             }
         }
-
-        //Close the program
-        scanner.close();
     }
 }
