53d48a860ed1bda8f1d5cfd48952c9ac3162d629
Jian Yang <126151808+jianyang999@users.noreply.github.com>
2026-08-28 03:38:10 +0800

    Extract Ui, Parser, TaskList, Storage for A-MoreOOP
    LeBron now constructs Ui/Storage/TaskList once and delegates user I/O
    to Ui, command interpretation and argument extraction to Parser, and
    task list/persistence to TaskList/Storage, instead of doing everything
    inline in main().

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -1,152 +1,89 @@
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class LeBron {
+    private Storage storage;
+    private TaskList taskList;
+    private Ui ui;
 
-    private static CommandType getCommandType(String input) {
-        String firstWord = input.split(" ")[0];
-        switch (firstWord) {
-            case "bye":
-                return CommandType.BYE;
-            case "list":
-                return CommandType.LIST;
-            case "mark":
-                return CommandType.MARK;
-            case "unmark":
-                return CommandType.UNMARK;
-            case "todo":
-                return CommandType.TODO;
-            case "deadline":
-                return CommandType.DEADLINE;
-            case "event":
-                return CommandType.EVENT;
-            case "delete":
-                return CommandType.DELETE;
-            default:
-                return CommandType.UNKNOWN;
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Storage storage = new Storage();
-        TaskList taskList;
+    public LeBron() {
+        ui = new Ui();
+        storage = new Storage();
         try {
             taskList = storage.load();
         } catch (IOException e) {
-            System.out.println("    Couldn't load your saved grind list, starting fresh.");
+            ui.showMessage("Couldn't load your saved grind list, starting fresh.");
             taskList = new TaskList(new ArrayList<>());
         }
+    }
 
-        //Initial message
-        String banner = "____________________________________________________________\n" +
-                "What's up, I am LeBron.\n" +
-                "Can the king assist you in any way?\n" +
-                "____________________________________________________________";
-        System.out.println(banner);
-
-        while (true){
-            //Read user input
-            String input = scanner.nextLine();
-            CommandType commandType = getCommandType(input);
+    public void run() {
+        ui.showBanner();
+        while (true) {
+            String input = ui.readCommand();
+            CommandType commandType = Parser.getCommandType(input);
 
             try {
                 switch (commandType) {
                     case BYE -> {
-                        System.out.println("    " + "Peace out fam!\n" + "____________________________________________________________");
-                        scanner.close();
+                        ui.showMessage("Peace out see ya later!");
                         return;
                     }
                     case LIST -> {
-                        System.out.println(taskList);
-                        System.out.println("____________________________________________________________");
-                    }
-                    case MARK -> {
-                        int taskNumber = Integer.parseInt((input.split(" "))[1]);
-                        taskList.getTask(taskNumber).setStatus(true);
-                        System.out.println("    " +  "Oh yea we're striving for greatness!\n" + "    " + taskList.getTask(taskNumber));
-                        System.out.println("____________________________________________________________");
-                    }
-                    case UNMARK -> {
-                        int taskNumber = Integer.parseInt((input.split(" "))[1]);
-                        taskList.getTask(taskNumber).setStatus(false);
-                        System.out.println("    " +  "Oh nah we undoing stuff now?\n" + "    " + taskList.getTask(taskNumber));
-                        System.out.println("____________________________________________________________");
+                        ui.showTaskList(taskList);
                     }
                     case TODO -> {
-                        String description = input.length() > 4 ? input.substring(5).trim() : "";
-                        if (description.isEmpty()) {
-                            throw new LeBronException("Whatchu need to do?");
-                        }
-                        taskList.addTask(new Todo(description));
-                        System.out.println("    More todo!");
-                        System.out.println("    " + taskList.getTask(taskList.size()));
-                        System.out.println("    " + taskList.size() + " tasks in your grind list now!");
-                        System.out.println("____________________________________________________________");
+                        String description = Parser.parseTodoDescription(input);
+                        Task task = new Todo(description);
+                        taskList.addTask(task);
+                        ui.showTodoAdded(task, taskList.size());
                     }
                     case DEADLINE -> {
-                        String fullDesc = input.length() > 8 ? input.substring(9).trim() : "";
-                        if (fullDesc.isEmpty()) {
-                            throw new LeBronException("Yo specify your deadline!");
-                        }
-                        if (!fullDesc.contains(" by ")) {
-                            throw new LeBronException("Do not test me kid, specify your due date!");
-                        }
-                        String[] parts = fullDesc.split(" by ");
-                        taskList.addTask(new Deadline(parts[0], LocalDateTime.parse(parts[1].trim(), DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"))));
-                        System.out.println("    " + "Deadlines forge kings!");
-                        System.out.println("    " + taskList.getTask(taskList.size()));
-                        System.out.println("    " + taskList.size() + " tasks in your grind list now!");
-                        System.out.println("____________________________________________________________");
+                        Parser.DeadlineArgs args = Parser.parseDeadlineArgs(input);
+                        Task task = new Deadline(args.description(), args.by());
+                        taskList.addTask(task);
+                        ui.showDeadlineAdded(task, taskList.size());
                     }
                     case EVENT -> {
-                        String fullDesc = input.length() > 5 ? input.substring(6).trim() : "";
-                        if (fullDesc.isEmpty()) {
-                            throw new LeBronException("What event you tryna do?");
-                        }
-                        if (!fullDesc.contains(" from ") || !fullDesc.contains(" to ")) {
-                            throw new LeBronException("Please tell me when it starts and ends little one.");
-                        }
-                        String[] parts = fullDesc.split(" from | to ");
-                        taskList.addTask(new Event(parts[0], LocalDateTime.parse(parts[1].trim(), DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm")),
-                                LocalDateTime.parse(parts[2].trim(), DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"))));
-                        System.out.println("    " + "Event fit for a king!");
-                        System.out.println("    " + taskList.getTask(taskList.size()));
-                        System.out.println("    " + taskList.size() + " tasks in your grind list now!");
-                        System.out.println("____________________________________________________________");
+                        Parser.EventArgs args = Parser.parseEventArgs(input);
+                        Task task = new Event(args.description(), args.start(), args.end());
+                        taskList.addTask(task);
+                        ui.showEventAdded(task, taskList.size());
+                    }
+                    case MARK -> {
+                        Task task = taskList.getTask(Parser.parseTaskNumber(input));
+                        task.setStatus(true);
+                        ui.showTaskMarked(task);
+                    }
+                    case UNMARK -> {
+                        Task task = taskList.getTask(Parser.parseTaskNumber(input));
+                        task.setStatus(false);
+                        ui.showTaskUnmarked(task);
                     }
                     case DELETE -> {
-                        int taskNumber = Integer.parseInt((input.split(" "))[1]);
-                        Task removed = taskList.getTask(taskNumber);
-                        taskList.deleteTask(taskNumber);
-                        System.out.println("    Task been taken care of!");
-                        System.out.println("    " + removed);
-                        System.out.println("    " + taskList.size() + " tasks left kiddo.");
-                        System.out.println("____________________________________________________________");
+                        Task task = taskList.getTask(Parser.parseTaskNumber(input));
+                        taskList.deleteTask(Parser.parseTaskNumber(input));
+                        ui.showTaskDeleted(task, taskList.size());
                     }
                     default -> throw new LeBronException("Whatchu tryna do youngblood?");
                 }
                 storage.save(taskList);
             } catch (LeBronException e) {
-                System.out.println("    " + e.getMessage());
-                System.out.println("____________________________________________________________");
+                ui.showMessage(e.getMessage());
             } catch (NumberFormatException e) {
-                System.out.println("    This task don't exist, don't piss me off.");
-                System.out.println("____________________________________________________________");
+                ui.showMessage("This task don't exist, don't play with me!.");
             } catch (IndexOutOfBoundsException e) {
-                System.out.println("    This task don't exist, don't piss me off.");
-                System.out.println("____________________________________________________________");
+                ui.showMessage("This task don't exist, don't play with me!.");
             } catch (IOException e) {
-                System.out.println("    Couldn't save your grind list, kid.");
-                System.out.println("____________________________________________________________");
+                ui.showMessage("Couldn't save your grind list.");
             } catch (DateTimeParseException e) {
-                System.out.println("    That date don't look right, use yyyy-MM-dd HHmm man, e.g. 2019-10-15 1800.");
-                System.out.println("____________________________________________________________");
+                ui.showMessage("That date don't look right, use yyyy-MM-dd HHmm man, e.g. 2019-10-15 1800.");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new LeBron().run();
     }
 }
